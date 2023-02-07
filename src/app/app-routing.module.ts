dd2468afe92f424cf0a61d68c9a7859e84865204
Srdjan Hajder <igorisrdjan@gmail.com>
2023-02-07 14:23:43 +0100

    Create tasks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TasksComponent } from './tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: 'zadaci',
+  //   component: TasksComponent
+  // },
+  {
+    path: 'novi-zadatak',
+    component: NewTaskComponent
+  },
+  {
+    path: '**',
+    component: TasksComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    NewTaskComponent
   ],
   imports: [
     BrowserModule,
